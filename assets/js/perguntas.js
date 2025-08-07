79cd4690fdb63dcec01f5757b2fad2a7dc2615dd
MatthsRaynor <dourado.sharon@escola.pr.gov.br>
2025-08-07 11:45:52 -0300

    Add files via upload

---
+++ assets/js/perguntas.js
@@ -0,0 +1,62 @@
+// Personagens
+criaCartao(
+    'Personagens',
+    'Quem é o líder da Agência de Detetives Armados?',
+    'Yukichi Fukuzawa é o líder da Agência de Detetives Armados.'
+);
+
+criaCartao(
+    'Personagens',
+    'Qual é a habilidade de Osamu Dazai?',
+    'Nenhum Mais Humano (人間失格, Ningen Shikkaku) - anula todas as habilidades que entram em contato com ele.'
+);
+
+criaCartao(
+    'Personagens',
+    'Qual é o verdadeiro nome de Chuuya Nakahara?',
+    'Seu verdadeiro nome é Chūya Nakahara, nomeado após o poeta japonês Chūya Nakahara.'
+);
+
+criaCartao(
+    'Personagens',
+    'Qual é a habilidade de Atsushi Nakajima?',
+    'Tigre Branco Sob o Luar (月下獣, Gekkajū) - transforma-se em um tigre branco.'
+);
+
+// Habilidades
+criaCartao(
+    'Habilidades',
+    'Qual é a habilidade de Ranpo Edogawa?',
+    'Super Dedução (超推理, Chō Suiri) - capacidade de dedução sobre-humana (embora ele afirme que não é uma habilidade).'
+);
+
+criaCartao(
+    'Habilidades',
+    'O que a habilidade "O Crime e o Castigo" faz?',
+    'Permite a Fyodor Dostoevsky matar qualquer pessoa que cometeu um crime apenas tocando-os.'
+);
+
+criaCartao(
+    'Habilidades',
+    'Qual é a habilidade de Akiko Yosano?',
+    'Thou Shalt Not Die (君死ニタマフ事ナカレ, Kimi Shinitamou Koto Nakare) - cura feridos quase mortos.'
+);
+
+// Organizações
+criaCartao(
+    'Organizações',
+    'Qual é o nome da organização criminosa que Chuuya e Dazai faziam parte?',
+    'Eles faziam parte da organização criminosa chamada "Máfia Portuária" (Port Mafia).'
+);
+
+criaCartao(
+    'Organizações',
+    'Qual organização é rival da Agência de Detetives Armados?',
+    'A Máfia Portuária é a principal rival da Agência de Detetives Armados.'
+);
+
+criaCartao(
+    'Curiosidades',
+    'Qual é a conexão entre os nomes dos personagens e a literatura?',
+    'Todos os personagens principais são nomeados após autores literários famosos e suas habilidades são nomeadas após suas obras.'
+);
